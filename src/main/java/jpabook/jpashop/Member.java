2d60a0c086bd648eebee5e61bd62a24272d86656
DongWoonKim <01030985843z@naver.com>
2022-11-08 12:58:18 +0900

    create entity class-1

--- src/main/java/jpabook/jpashop/Member.java
+++ src/main/java/jpabook/jpashop/Member.java
@@ -6,11 +6,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
-@Data
+//@Entity
+//@Data
 public class Member {
 
-    @Id @GeneratedValue
+//    @Id @GeneratedValue
     private Long id;
     private String username;
 
---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Data
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+}
